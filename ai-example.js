981f45154b8a9fc7f08f4022699a005a820996af
Jake Anderson <nate.and.mimsy@gmail.com>
2017-12-08 14:54:13 -0500

    ai example

---
+++ ai-example.js
@@ -0,0 +1,27 @@
+function myAI(gameboardState, currentBoard){
+	var turn = createTurn(-1, -1);
+	if (currentBoard == -1){
+		for (var i = 0; i < gameboardState.length; i++){
+			if(checkBoardWinner(gameboardState[i]) == 0){
+				turn.boardIndex = i;
+				currentBoard = i;
+				break;
+			}
+		}
+		for (var i = 0; i < gameboardState.length; i++){
+			if(gameboardState[currentBoard][i] == 0){
+				turn.spotIndex = i;
+				break;
+			}
+		}
+	}else{
+		turn.boardIndex = currentBoard;
+		for (var i = 0; i < gameboardState.length; i++){
+			if(gameboardState[currentBoard][i] == 0){
+				turn.spotIndex = i;
+				break;
+			}
+		}
+	}
+	return turn;
+}
