0f02267271deeeca94e47088467cc74f74e4eb26
Jake Anderson <nate.and.mimsy@gmail.com>
2017-12-09 15:38:30 -0500

    AInderson loses to randombot

--- AInderson.js
+++ AInderson.js
@@ -24,32 +24,97 @@
 //				spot must be equal to 0 in the gameboardState
 
 function myAI(gameboardState, gameboardsWon, currentBoardIndex, player){
-	var turn = Turn(-1, -1);
 	if (currentBoardIndex == -1){
-		for (var i = 0; i < gameboardState.length; i++){
-			if(gameboardsWon[i] == 0){
-				turn.boardIndex = i;
-				currentBoardIndex = i;
-				break;
-			}
-		}
-		for (var i = 0; i < gameboardState.length; i++){
-			if(gameboardState[currentBoardIndex][i] == 0){
-				turn.spotIndex = i;
-				break;
-			}
-		}
+		return playAnyBoard(gameboardState, gameboardsWon, player);
 	}else{
-		turn.boardIndex = currentBoardIndex;
-		for (var i = 0; i < gameboardState.length; i++){
-			if(gameboardState[currentBoardIndex][i] == 0){
-				turn.spotIndex = i;
-				break;
+		return playBoard(currentBoardIndex, gameboardState, gameboardsWon, player);
+	}
+}
+
+function playAnyBoard(gameboardState, gameboardsWon, player){
+
+	var vm = [];
+	var wm = [];
+	var wmtfcb = [];
+	var mtfcb = [];
+
+	for(var i = 0; i < gameboardsWon.length; i++){
+		if(gameboardsWon[i] == 0){
+			var validMoves = findAllValidMovesOnMiniboard(gameboardState[i], player);
+			var winningMoves = findWinningMovesOnMiniboard(validMoves, gameboardState[i], player);
+			var winnningMovesThatForceCertainBoard = findMovesThatForceCertainBoard(winningMoves, gameboardsWon, player);
+			var movesThatForceCertainBoard = findMovesThatForceCertainBoard(validMoves, gameboardsWon, player);
+
+			if (winnningMovesThatForceCertainBoard.length > 0){
+				wmtfcb.push(Turn(i, winnningMovesThatForceCertainBoard[0]));
+			}else if (winningMoves.length > 0){
+				wm.push(Turn(i, winningMoves[0]));
+			}else if (movesThatForceCertainBoard.length > 0){
+				mtfcb.push(Turn(i, movesThatForceCertainBoard[0]));
+			}else{
+				vm.push(Turn(i, validMoves[0]));
 			}
 		}
 	}
-	
-	return turn;
+
+	if(wmtfcb.length > 0){
+		return wmtfcb[0];
+	}else if (wm.length > 0){
+		return wm[0];
+	}else if (mtfcb.length > 0){
+		return mtfcb[0];
+	}else{
+		return vm[0];
+	}
+}
+
+function playBoard(boardIndex, gameboardState, gameboardsWon, player){
+	var validMoves = findAllValidMovesOnMiniboard(gameboardState[boardIndex], player);
+	var winningMoves = findWinningMovesOnMiniboard(validMoves, gameboardState[boardIndex], player);
+	var winnningMovesThatForceCertainBoard = findMovesThatForceCertainBoard(winningMoves, gameboardsWon, player);
+	var movesThatForceCertainBoard = findMovesThatForceCertainBoard(validMoves, gameboardsWon, player);
+
+	if (winnningMovesThatForceCertainBoard.length > 0){
+		return Turn(boardIndex, winnningMovesThatForceCertainBoard[0]);
+	}else if (winningMoves.length > 0){
+		return Turn(boardIndex, winningMoves[0]);
+	}else if (movesThatForceCertainBoard.length > 0){
+		return Turn(boardIndex, movesThatForceCertainBoard[0]);
+	}else{
+		return Turn(boardIndex, validMoves[0]);
+	}
+}
+
+function findAllValidMovesOnMiniboard (board, player) {
+	var returnValue = [];
+	for (var i = 0; i < board.length; i++) {
+		if (board[i] == 0) {
+			returnValue.push(i);
+		}
+	}
+	return returnValue;
+}
+
+function findWinningMovesOnMiniboard (validMoves, board, player) {
+	var returnValue = [];
+	for (var i = 0; i < validMoves.length; i++) {
+		var testBoard = board.slice();
+		testBoard[validMoves[i]] = player;
+		if(checkBoardWinner(testBoard) == player) {
+			returnValue.push(validMoves[i]);
+		}
+	}
+	return returnValue;
+}
+
+function findMovesThatForceCertainBoard(validMoves, gameboardsWon, player) {
+	var returnValue = [];
+	for (var i = 0; i < validMoves.length; i++) {
+		if(gameboardsWon[validMoves[i]] == 0){
+			returnValue.push(validMoves[i]);
+		}
+	}
+	return returnValue;
 }
 
 //function Turn(boardIndex, spotIndex);
