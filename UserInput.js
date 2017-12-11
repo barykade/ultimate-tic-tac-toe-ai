70915d3a6b34f9c2f1467423d8054b24912d2e54
tfinateri <tfinater@umich.edu>
2017-12-10 20:47:58 -0500

    press enter key to submit user choice

--- UserInput.js
+++ UserInput.js
@@ -1,8 +1,11 @@
+var textbox;
+var textBoxReady = false;
+
 function myAI(gameboardState, gameboardsWon, currentBoard, myTeam){
 	return new Promise(async function(accept, reject) {
 		var turn = createTurn(-1, -1);
-		textboxDiv = document.createElement('div');
-		textboxPrompt = document.createElement('p');
+		var textboxDiv = document.createElement('div');
+		var textboxPrompt = document.createElement('p');
 		textbox = document.createElement('input');
 		textbox.type = "text";
 		textboxDiv.appendChild(textboxPrompt);
@@ -10,35 +13,36 @@ function myAI(gameboardState, gameboardsWon, currentBoard, myTeam){
 		document.body.insertBefore(textboxDiv, document.body.childNodes[0]);
 		textbox.focus();
 		var miniBoard = currentBoard;
+		textbox.addEventListener("keydown", function (event) {
+				if(event.key === 'Enter') {
+				    textBoxReady = true;
+				}
+			});
 		if (currentBoard == -1) {
 			textboxPrompt.innerHTML = "Which mini board would you like? (1-9)";
-			var textEntered = false;
-			while (!textEntered) {
-				if (textbox.value !== "") {
-					miniBoard = textbox.value - 1;
-					textbox.value = "";
-					textEntered = true;
-				}
-				await sleep(100);
-			}
+			miniBoard = await getUserInput();
 		}
 		textboxPrompt.innerHTML = "Which slot would you like? (1-9)";
-		var textEntered = false;
-		var slotIndex = -1;
-		while (!textEntered) {
-			if (textbox.value !== "") {
-				slotIndex = textbox.value - 1;
-				textbox.value = "";
-				textEntered = true;
-			}
-			await sleep(100)
-		}
+		var slotIndex = await getUserInput();
 		turn = createTurn(miniBoard, slotIndex);
 		document.body.removeChild(textboxDiv);
 		accept(turn);
 	}).then(function(result) {
 		return result;
 	})
+}
+
+async function getUserInput() {
+	var result;
+	while (textBoxReady == false) {
+		await sleep(100);
+		if (textBoxReady !== false) {
+			result = textbox.value - 1;
+		}
+	}
+	textBoxReady = false;
+	textbox.value = "";
+	return result;
 }
 
 function createTurn(boardIndex, spotIndex) {
